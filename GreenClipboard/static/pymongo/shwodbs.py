41320c0d26dd79efa30d0d27cfdeadd638cf3d3e
GreenCat <90766106+GreeenCat@users.noreply.github.com>
2023-04-12 15:59:01 +0530

    Delete GreenClipboard directory

--- GreenClipboard/static/pymongo/shwodbs.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-if __name__=='__main__':
-    client =pymongo.MongoClient('mongodb://localhost:27017')
-    print(client)
-    db = client['madhav']
-    collection = db['Thisistryal']
-    a = client.list_database_names()
-    print(a)
-    
